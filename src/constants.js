56de1065258202bde18c6c9887b588c92bc5311b
gyeshinwon <bluemirr5@gmail.com>
2017-03-04 20:13:09 +0900

    init

--- src/constants.js
+++ src/constants.js
@@ -5,11 +5,11 @@
 
 export default {
   firebaseConfig : {
-    apiKey: "AIzaSyCYekDhHPovhYGHozvyIdXfBiNceHJvIIg",
-    authDomain: "goodhous-e9b35.firebaseapp.com",
-    databaseURL: "https://goodhous-e9b35.firebaseio.com",
-    storageBucket: "goodhous-e9b35.appspot.com",
-    messagingSenderId: "365981659012"
+    apiKey: "xxxxx",
+    authDomain: "xxxxx",
+    databaseURL: "xxxx",
+    storageBucket: "xxxx",
+    messagingSenderId: "xxxxx"
   }
 }
 
